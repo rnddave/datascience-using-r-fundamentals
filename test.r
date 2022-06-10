545398d1c96d229ff5ac575947aa2b483ad0057f
David Dickinson <rnddave@gmail.com>
2022-06-10 17:25:25 +0100

    just trying some very simple commands in r

---
+++ test.r
@@ -0,0 +1,18 @@
+1+1
+
+# rscript file.r to run from osx
+
+c(42, 33, 4, 1) -> myFamilyAges
+# this will assign the values to my newly named variable on the fly
+
+myFamilyAges
+# and this will print it, from this point of view, this looks even easier to learn than Python is for basic syntax. 
+
+sum(myFamilyAges)
+# 80
+
+mean(myFamilyAges)
+range(myFamilyAges)
+
+
+
